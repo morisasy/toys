ec084b0909fd0f962796fc91868d6a9ff886f900
Medi Risasi <morisasy@gmail.com>
2018-10-02 23:05:47 +0300

    Initial

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_modus import Modus
+from toy import Toy
+
+app = Flask(__name__)
+modus = Modus(app)
+
+duplo = Toy(name='duplo')
+lego = Toy(name='lego')
+knex = Toy(name='knex')
+
+toys = [duplo,lego,knex]
+
+@app.route('/toys', methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        toys.append(Toy(request.form['name']))
+        return redirect(url_for('index'))
+    return render_template('index.html', toys=toys)
+
+@app.route('/toys/new')
+def new():
+    return render_template('new.html')
+    
+@app.route('/toys/<int:id>', methods=["GET", "PATCH", "DELETE"])
+def show(id):
+    # Refactored using a generator so that we do not need to do [0]!
+    found_toy = next(toy for toy in  toys if toy.id == id)
+
+    # if we are updating a toy...
+    if request.method == b"PATCH":
+        found_toy.name = request.form['name']
+        return redirect(url_for('index'))
+
+    if request.method == b"DELETE":
+        toys.remove(found_toy)
+        return redirect(url_for('index'))
+    # if we are showing information about a toy
+    return render_template('show.html', toy=found_toy)
+
+@app.route('/toys/<int:id>/edit')
+def edit(id):
+    # Refactored using a generator so that we do not need to do [0]!
+    found_toy = next(toy for toy in  toys if toy.id == id)
+    return render_template('edit.html', toy=found_toy)
